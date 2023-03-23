737c2bcb83f53457dffb04efba677ecb2311f5f0
Igor Andrushenko <igorandrushenko2001@gmail.com>
2023-03-23 15:55:42 +1000

    add Example005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "игорь")
+{
+    Console.WriteLine("Ура, это опять ты. Чё пришёл?"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
